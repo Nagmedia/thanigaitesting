f32085ef6af70a8769bc63a24706df1641c0f48c
Nagmedia <nagmedia2024@gmail.com>
2025-11-25 12:37:06 +0530

    Add files via upload

---
+++ framercanvas.com/s/app.d9486be99a7c5d229e4db4b1b7c37a0c0e73d065/scripts/react-jsx-runtime.esm.js
@@ -0,0 +1,3 @@
+export const { Fragment, jsx, jsxs, jsxDEV } = window.__REACT_RUNTIME_JSX__
+
+//# sourceURL=https://framercanvas.com/s/app.d9486be99a7c5d229e4db4b1b7c37a0c0e73d065/scripts/react-jsx-runtime.esm.js
---
+++ framercanvas.com/s/app.d9486be99a7c5d229e4db4b1b7c37a0c0e73d065/scripts/react.esm.js
@@ -0,0 +1,43 @@
+const React = window.React
+// export * from "react"
+export const {
+    Children,
+    Component,
+    Fragment,
+    Profiler,
+    PureComponent,
+    StrictMode,
+    Suspense,
+    __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
+    cloneElement,
+    createContext,
+    createElement,
+    createFactory,
+    createRef,
+    forwardRef,
+    isValidElement,
+    lazy,
+    memo,
+    startTransition,
+    unstable_act,
+    useCallback,
+    useContext,
+    useDebugValue,
+    useDeferredValue,
+    useEffect,
+    useId,
+    useImperativeHandle,
+    useInsertionEffect,
+    useLayoutEffect,
+    useMemo,
+    useReducer,
+    useRef,
+    useState,
+    useSyncExternalStore,
+    useTransition,
+    version,
+} = React
+// export default from "react"
+export default React
+
+//# sourceURL=https://framercanvas.com/s/app.d9486be99a7c5d229e4db4b1b7c37a0c0e73d065/scripts/react.esm.js
